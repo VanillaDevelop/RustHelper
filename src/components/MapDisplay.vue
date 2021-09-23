2d27fc360f233e124ba95e32fc5f751b8b6c3100
Vanilla <vanilla@nya.gg>
2021-09-23 18:10:13 +0200

    Show monuments on map using markerjs

--- src/components/MapDisplay.vue
+++ src/components/MapDisplay.vue
@@ -15,23 +15,21 @@ a {
 }
 
 #mapImage {
-    width:100%;
-    height: 100%;
-    margin-top:20px;
+  margin-top: 20px;
 }
 </style>
 
 <script>
 import { mapGetters } from 'vuex';
-import * as markerjs2 from 'markerjs2';
+import * as mjslive from 'markerjs-live';
 
 export default
   {
     data: function ()
     {
-        return {
+      return {
 
-        }
+      }
     },
     computed:
     {
@@ -43,8 +41,40 @@ export default
     },
     mounted()
     {
-        let markerArea = new markerjs2.MarkerArea(document.getElementById('mapImage'))
-        markerArea.show();
+      let config_markers = [];
+      this.currentServer.mapStatus.monuments.forEach(monument => {
+        if(!(monument.monument.startsWith("Powerline") || monument.monument.startsWith("Tunnel_Entrance") || monument.monument.startsWith("Iceberg") || monument.monument.startsWith("Power_Substation") ||
+                monument.monument.startsWith("Swamp")))
+        {
+          config_markers.push(
+            {
+                  fillColor: "transparent",
+                  strokeColor: "#FFFF00",
+                  strokeWidth: 3,
+                  strokeDasharray: "3",
+                  opacity: 1,
+                  left: monument.x - 25,
+                  top: (this.currentServer.mapStatus.size)/2 - monument.y - 25,
+                  width: 50,
+                  height: 50,
+                  rotationAngle: 0,
+                  visualTransformMatrix: { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 },
+                  containerTransformMatrix: { a: 1, b: 0, c: 0, d: 1, e: 0, f: 0 },
+                  typeName: "EllipseFrameMarker",
+                  state: "select"
+            }
+          )
+        }
+      })
+
+      let config = {
+        width: this.currentServer.mapStatus.size/2,
+        height: this.currentServer.mapStatus.size/2,
+        markers: config_markers
+      }
+      let target = document.getElementById('mapImage');
+      let markerView = new mjslive.MarkerView(target);
+      markerView.show(config);
     }
   }
 </script>
